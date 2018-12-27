bbf7f9a5cf066f76af0e79457789313422e8ee0f
Eilif Johansen <eilif@eilifjohansen.no>
2018-12-27 17:34:01 +0100

    Added Plesk to the title on frontpage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel is Awesome!
+                    Laravel Plesk!
                 </div>
 
                 <div class="links">
